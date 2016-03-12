86acdde86dd783832a2f55dd8a717afa5eb5618d
Kirk <kkchu791@gmail.com>
2016-03-12 02:07:53 -0800

    update main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,2 +1,12 @@
 class MainController < ApplicationController
+  def ping
+    render json: {
+      data: [{
+        type: "responses",
+        attributes: {
+          pong: true
+        }
+      }]
+    }
+  end
 end
