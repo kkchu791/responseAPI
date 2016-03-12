2c1570f5d6c8866953deea676fe1c96e87de749d
Kirk <kkchu791@gmail.com>
2016-03-12 12:05:55 -0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-users = User.create!([{email: "user_one@example.com", password_hash: "secret"}])
+users = User.create!([{email: "user_one@example.com", password: "secret"}])
 responses = Response.create!([{reply:"Crikey Mate"},
   {reply:"Aye Yo That's a Big Gorilla"}])
 
