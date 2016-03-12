5e1b68e9019811ce18ec41cededd11ccdc4749eb
Kirk <kkchu791@gmail.com>
2016-03-12 10:50:17 -0800

    Change development db back to sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160312102747) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "responses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
