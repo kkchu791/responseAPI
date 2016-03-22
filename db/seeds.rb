cc09a62f7fa16f4651d88042f741748a03f5d8ad
Kirk <kkchu791@gmail.com>
2016-03-22 15:14:07 -0700

    Update seed file with eightball outputs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,23 @@
 users = User.create!([{email: "user_one@example.com", password: "secret"}])
-responses = Response.create!([{reply:"Crikey Mate"},
-  {reply:"Aye Yo That's a Big Gorilla"}])
+responses = Response.create!([{reply:"It is certain"},
+  {reply:"It is decidedly so"},
+  {reply:"Without a doubt"},
+  {reply:"Yes, definitely"},
+  {reply:"You may rely on it"},
+  {reply:"As I see it, yes"},
+  {reply:"Most likely"},
+  {reply:"Outlook good"},
+  {reply:"Yes"},
+  {reply:"All Signs point to yes!"},
+  {reply:"Reply hazy try again"},
+  {reply:"Ask again later"},
+  {reply:"Better not tell you now"},
+  {reply:"Cannot predict now"},
+  {reply:"Concentrate and ask again"},
+  {reply:"Don't count on it"},
+  {reply:"My reply is no"},
+  {reply:"My sources say no"},
+  {reply:"Outlook not so good"},
+  {reply:"Very doubtful"}
+  ])
 
