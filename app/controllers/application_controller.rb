2369f7f789c781b36dc38b953fb0c4bdc87c2b39
Kirk <kkchu791@gmail.com>
2016-04-17 23:08:02 -0700

    Remove ensure current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  # def restrict_access
-  #   authenticate_or_request_with_http_token do |token, options|
-  #     ApiKey.exists?(access_token: token)
-  #   end
-  # end
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiKey.exists?(access_token: token)
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :ensure_current_user
-
   def create
     user = User.new(user_params)
     if user.save
