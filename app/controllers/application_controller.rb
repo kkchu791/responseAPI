391a481b21f82bbf6f738ffeeb0d09d0dcac21ba
Kirk <kkchu791@gmail.com>
2016-04-25 18:07:17 -0700

    Update restrict access for app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  before_filter :restrict_access
 
   def restrict_access
     authenticate_or_request_with_http_token do |token, options|
